99edde0e30f2870704bdfbd022b17bc59a059e42
wildenali <wildenwildenaliali@gmail.com>
2020-10-02 22:33:59 +0700

    ganti ssid dan password

--- 13-HTTP_POST_Requests/13A-HTTP_POST_Reqest_Basic/13A-HTTP_POST_Reqest_Basic.ino
+++ 13-HTTP_POST_Requests/13A-HTTP_POST_Reqest_Basic/13A-HTTP_POST_Reqest_Basic.ino
@@ -1,8 +1,8 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 
-const char* ssid = "101 residance_fm";
-const char* password = "101littleangel";
+const char* ssid = "inimodem";
+const char* password = "inimodem";
 
 void setup () {
   Serial.begin(115200);
