5c4828ddb52961930a2734608aa70482fd63cb4f
wildenali <wildenwildenaliali@gmail.com>
2020-10-02 16:47:04 +0700

    http post request, cuma belum di test

---
+++ 13-HTTP_POST_Requests/13A-HTTP_POST_Reqest_Basic/13A-HTTP_POST_Reqest_Basic.ino
@@ -0,0 +1,37 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+
+const char* ssid = "101 residance_fm";
+const char* password = "101littleangel";
+
+void setup () {
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.print("Connecting..");
+  }
+}
+
+void loop() {
+
+  if (WiFi.status() == WL_CONNECTED) { //Check WiFi connection status
+    HTTPClient http;    //Declare object of class HTTPClient
+    http.begin("http://192.168.1.88:8085/hello");      //Specify request destination
+    http.addHeader("Content-Type", "text/plain");  //Specify content-type header
+
+    int httpCode = http.POST("Message from ESP8266");   //Send the request
+    String payload = http.getString();                  //Get the response payload
+
+    Serial.println(httpCode);   //Print HTTP return code
+    Serial.println(payload);    //Print request response payload
+
+    http.end();  //Close connection
+  } else {
+    Serial.println("Error in WiFi connection");
+  }
+
+  delay(5000);  //Send a request every 30 seconds
+
+}
---
+++ 13-HTTP_POST_Requests/README.md
@@ -0,0 +1,3 @@
+Reference:
+
+- [https://techtutorialsx.com/2016/07/21/esp8266-post-requests/](https://techtutorialsx.com/2016/07/21/esp8266-post-requests/)
